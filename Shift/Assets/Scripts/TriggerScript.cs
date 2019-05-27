ad53dadd105faed0523ef412b40892d59c84ec0d
ZakBrown1998 <43067354+ZakBrown1998@users.noreply.github.com>
2019-05-27 17:19:31 +0100

    Time Travel Mechanic implemented and new animations added for backgroudn appearing and disappearing.
    The time travel mechanic has been built and implemented into the caves stage. New animations have been added to make the background disappear and appear when the jewel is collided with.

---
+++ Shift/Assets/Scripts/TriggerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    //makes the animator visible in the inspector 
+    [SerializeField] private Animator timeJewelController;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //this is stops the boulder from moving when colliding with the trigger
+        if (other.CompareTag("Player"))
+            timeJewelController.SetBool("ChangeBackground", true);
+    }
+
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        //this is if the boulder hasnt hit the trigger and the animation still plays
+        if (other.CompareTag("Player"))
+            timeJewelController.SetBool("ChangeBackground", false);
+    }
+}
